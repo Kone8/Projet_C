78394aa4ca1b7d06d163412e09c2fd5c126c214a
baptistenauche1508 <167711009+baptistenauche1508@users.noreply.github.com>
2024-05-20 20:16:12 +0200

    Update column.c

--- column.c
+++ column.c
@@ -1,1 +1,30 @@
+#include "column.h"
+
+// Crée une colonne avec le titre spécifié
+Column* create_column(const char* title) {
+    Column* column = (Column*)malloc(sizeof(Column));
+    if (!column) {
+        fprintf(stderr, "impossible d'allouer la memoire pour la colonne\n");
+        return NULL;
+    }
+    column->title = strdup(title);
+    if (!column->title) {
+        fprintf(stderr, " impossible d'allouer la memoire pour le titre de colonne\n");
+        free(column);
+        return NULL;
+    }
+    column->data = NULL;
+    column->type = -1;
+    column->num_rows = 0;
+    return column;
+}
+
+// Supprime une colonne et libère la mémoire associée
+void delete_column(Column** column) {
+    if (column && *column) {
+        free((*column)->title);
+        free(*column);
+        *column = NULL;
+    }
+}
 
